618b2f292069a94034802de736f25b15c3f1f33e
DaiyanB <76210905+DaiyanB@users.noreply.github.com>
2022-01-02 12:05:26 +0000

    Updated urls.py and admin.py
    Registered Question model on admin.py and updated urls.py to remove an empty line

--- LogicByteBackend/api/urls.py
+++ LogicByteBackend/api/urls.py
@@ -4,7 +4,6 @@ from .views import *
 app_name = "api"
 
 urlpatterns = [
-
     path("api_profiles/", UserProfileList.as_view()),
     path("api_profiles/<str:field_name>=<str:field_value>", UserProfileByField.as_view()),
     path("api_questions/", QuestionList.as_view()),
