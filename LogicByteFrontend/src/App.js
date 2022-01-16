29e4f3f96f1648aa525e9df6424c5d5eb02c8f12
Dinu-Filip <dinu.filip.self@gmail.com>
2022-01-16 16:23:15 +0000

    Moved router to app.js

--- LogicByteFrontend/src/App.js
+++ LogicByteFrontend/src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
-import MainNavBar from "./Components/MainNavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/"></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- LogicByteFrontend/src/index.js
+++ LogicByteFrontend/src/index.js
@@ -2,13 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App />}></Route>
-        </Routes>
-    </BrowserRouter>,
-    document.getElementById("root")
-);
+ReactDOM.render(<App></App>, document.getElementById("root"));
