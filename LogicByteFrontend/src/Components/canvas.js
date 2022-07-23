25f8517c806ff7ad38a3377fc50b0d694538d0f9
alunity <75143943+alunity@users.noreply.github.com>
2022-07-23 22:06:35 +0100

    Optimised canvas

--- LogicByteFrontend/src/Components/canvas.js
+++ LogicByteFrontend/src/Components/canvas.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { getStroke } from "perfect-freehand";
 import "./canvas.css";
 import MultiSelect from "./multiSelect";
+import PropTypes from "prop-types";
 
 function getSvgPathFromStroke(stroke) {
   if (!stroke.length) return "";
@@ -19,39 +20,39 @@ function getSvgPathFromStroke(stroke) {
   return d.join(" ");
 }
 
-// var options = {
-//   size: 10,
-//   thinning: 0.5,
-//   smoothing: 0.5,
-//   streamline: 0.5,
-//   easing: (t) => t,
-//   start: {
-//     taper: 0,
-//     easing: (t) => t,
-//     cap: true,
-//   },
-//   end: {
-//     taper: 100,
-//     easing: (t) => t,
-//     cap: true,
-//   },
-// };
-
 var options = {
-  size: 16,
-  smoothing: 0.5,
+  size: 10,
   thinning: 0.5,
+  smoothing: 0.5,
   streamline: 0.5,
   easing: (t) => t,
   start: {
     taper: 0,
+    easing: (t) => t,
     cap: true,
   },
   end: {
-    taper: 0,
+    taper: 100,
+    easing: (t) => t,
     cap: true,
   },
 };
+
+// var options = {
+//   size: 16,
+//   smoothing: 0.5,
+//   thinning: 0.5,
+//   streamline: 0.5,
+//   easing: (t) => t,
+//   start: {
+//     taper: 0,
+//     cap: true,
+//   },
+//   end: {
+//     taper: 0,
+//     cap: true,
+//   },
+// };
 
 function getClassNameForColour(colour) {
   switch (colour) {
@@ -68,68 +69,34 @@ function getClassNameForColour(colour) {
   }
 }
 
-function Canvas() {
-  const colours = ["Black", "Blue", "Green", "Red", "Yellow"];
-
-  // points is the line the user is currently drawing
+function CurrentLine(props) {
   const [points, setPoints] = useState({ points: [] });
-  // Strokes contains all of the lines that the user has drawn
-  const [strokes, setStrokes] = useState(restoreLocalStorage());
-  const [isDrawing, setIsDrawing] = useState(false);
-  const [thickness, setThickness] = useState(20);
-  const [colour, setColour] = useState(colours[0]);
 
   function handlePointerDown(e) {
-    if (isDrawing) {
-      // setPoints([[e.clientX, e.clientY, e.pressure]]);
+    if (props.isDrawing) {
       setPoints({
-        colour: colour,
-        thickness: thickness,
+        colour: props.colour,
+        thickness: props.thickness,
         points: [[e.clientX, e.clientY, e.pressure]],
       });
     }
   }
-
-  function restoreLocalStorage() {
-    if (localStorage.canvas != undefined) {
-      let x = JSON.parse(localStorage.canvas);
-      return x;
-    } else {
-      // console.log("No data");
-      return [];
-    }
-  }
-
-  function saveLocalStorage() {
-    let data = JSON.stringify(strokes);
-    console.log("saving");
-    localStorage.canvas = data;
-  }
-
   function handlePointerMove(e) {
-    if (e.buttons !== 1 || isDrawing == false) return;
+    if (e.buttons !== 1 || props.isDrawing == false) return;
     // setPoints([...points, [e.clientX, e.clientY, e.pressure]]);
     setPoints({
-      colour: colour,
-      thickness: thickness,
+      colour: props.colour,
+      thickness: props.thickness,
       points: [...points.points, [e.clientX, e.clientY, e.pressure]],
     });
   }
 
   function handlePointerUp() {
-    if (isDrawing) {
-      setStrokes([...strokes, points]);
+    if (props.isDrawing) {
+      // props.setStrokes([...strokes, points]);
+      props.setStrokes(points);
       setPoints({ points: [] });
     }
-  }
-
-  function clearCanvas() {
-    setStrokes([]);
-    setPoints({ points: [] });
-  }
-
-  function undo() {
-    setStrokes(strokes.slice(0, strokes.length - 2));
   }
 
   useEffect(() => {
@@ -143,6 +110,59 @@ function Canvas() {
       document.onpointerup = null;
     };
   });
+
+  options.size = points.thickness;
+  const stroke = getStroke(points.points, options);
+
+  const pathData = getSvgPathFromStroke(stroke);
+  let colourClassName = getClassNameForColour(points.colour);
+
+  return <path d={pathData} className={colourClassName}></path>;
+}
+
+CurrentLine.propTypes = {
+  isDrawing: PropTypes.bool,
+  colour: PropTypes.string,
+  thickness: PropTypes.string,
+  setStrokes: PropTypes.func,
+};
+
+function Canvas() {
+  const colours = ["Black", "Blue", "Green", "Red", "Yellow"];
+
+  // Strokes contains all of the lines that the user has drawn
+  const [strokes, setStrokes] = useState(restoreLocalStorage());
+  const [isDrawing, setIsDrawing] = useState(false);
+  // The slider returns a string, this keeps types consistent
+  const [thickness, setThickness] = useState("20");
+  const [colour, setColour] = useState(colours[0]);
+
+  function restoreLocalStorage() {
+    if (localStorage.canvas != undefined) {
+      let x = JSON.parse(localStorage.canvas);
+      return x;
+    } else {
+      // console.log("No data");
+      return [];
+    }
+  }
+
+  function saveLocalStorage() {
+    let data = JSON.stringify(strokes);
+    localStorage.canvas = data;
+  }
+
+  function clearCanvas() {
+    setStrokes([]);
+  }
+
+  function undo() {
+    setStrokes(strokes.slice(0, strokes.length - 2));
+  }
+
+  function handleSetStrokes(points) {
+    setStrokes([...strokes, points]);
+  }
 
   // Only saves sketch to localstorage when strokes is updated
   useEffect(() => saveLocalStorage(), [strokes]);
@@ -158,12 +178,6 @@ function Canvas() {
     let pathData = getSvgPathFromStroke(stroke);
     return <path key={i - 1} d={pathData} className={className}></path>;
   });
-
-  options.size = points.thickness;
-  const stroke = getStroke(points.points, options);
-
-  const pathData = getSvgPathFromStroke(stroke);
-  let colourClassName = getClassNameForColour(points.colour);
 
   return (
     <>
@@ -196,7 +210,12 @@ function Canvas() {
       <button onClick={undo}> Undo</button>
       <svg>
         {otherStrokes}
-        <path d={pathData} className={colourClassName}></path>
+        <CurrentLine
+          isDrawing={isDrawing}
+          colour={colour}
+          thickness={thickness}
+          setStrokes={handleSetStrokes}
+        />
       </svg>
     </>
   );
